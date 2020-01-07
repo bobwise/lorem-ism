0cfe2e82eea38a17c6dbfb95850201e5e399bb21
Bob Wise <bobwise@quickenloans.com>
2020-01-07 15:40:51 -0500

    accidentally merged in kitchen-sink

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <main  class="sprk-o-CenteredColumn sprk-u-ptl">
+      <h1 class="sprk-b-TypeDisplayTwo sprk-b-PageTitle">Hello, Spark! 👋</h1>
+      <h1 class="sprk-b-TypeDisplaySix sprk-u-mtl">Add components here</h1>
+    </main>
+  );
+}
+
+export default App;
--- src/containers/App/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import '@sparkdesignsystem/spark/es5/sparkPolyfills';
-import 'whatwg-fetch';
-import Router from '../../routes/Router';
-import '../../styles.scss';
-
-const App = () => <Router />;
-
-export default App;
