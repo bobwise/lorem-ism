2d822ee8cbc1273ab5f856c0bc34d80de7362ad6
Bob Wise <bobwise@quickenloans.com>
2020-01-10 13:06:29 -0500

    add font setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.scss';
+import IconSet from './components/IconSet/IconSet';
 
 function App() {
   return (
-    <main  class="sprk-o-CenteredColumn sprk-u-ptl">
-      <h1 class="sprk-b-TypeDisplayTwo sprk-b-PageTitle">Hello, Spark! 👋</h1>
-      <h1 class="sprk-b-TypeDisplaySix sprk-u-mtl">Add components here</h1>
+    <main className="sprk-o-CenteredColumn sprk-u-ptl">
+      <IconSet></IconSet>
+      <h1 className="sprk-b-TypeDisplayTwo sprk-b-PageTitle">Hello, Spark! 👋</h1>
+      <h1 className="sprk-b-TypeDisplaySix sprk-u-mtl">Add components here</h1>
     </main>
   );
 }
